c66cccae55897109b17b25fdf9447440a2759e3c
André Domingues <andremrmd@ua.pt>
2023-01-19 04:56:01 +0000

    .

---
+++ scripts.js
@@ -0,0 +1,335 @@
+console.log("im in")
+
+
+window.onload = function() {
+    document.getElementById("treze0").style.display="flex";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+};
+
+function zero(){
+
+    document.getElementById("treze0").style.display="flex";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+
+function primeiro(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="flex";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+}
+
+function segundo(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="flex";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function terceiro(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="flex";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function quarto(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="flex";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+
+function quinta(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="flex";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function sexta(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="flex";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function setima(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="flex";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function oitava(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="flex";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+
+}
+
+function nona(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="flex";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+}
+
+function dez(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="flex";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+}
+
+function onze(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="flex";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="none";
+
+
+
+
+}
+
+function doze(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="flex";
+    document.getElementById("treze13").style.display="none";
+
+
+
+}
+
+function treze(){
+
+    document.getElementById("treze0").style.display="none";
+    document.getElementById("treze1").style.display="none";
+    document.getElementById("treze2").style.display="none";
+    document.getElementById("treze3").style.display="none";
+    document.getElementById("treze4").style.display="none";
+    document.getElementById("treze5").style.display="none";
+    document.getElementById("treze6").style.display="none";
+    document.getElementById("treze7").style.display="none";
+    document.getElementById("treze8").style.display="none";
+    document.getElementById("treze9").style.display="none";
+    document.getElementById("treze10").style.display="none";
+    document.getElementById("treze11").style.display="none";
+    document.getElementById("treze12").style.display="none";
+    document.getElementById("treze13").style.display="flex";
+
+
+}
